7dcf5b95a0b09dcc2c3b79f8b7d82b9cb6081678
gitika0612 <bhatiagitika0612@gmail.com>
2020-12-23 14:44:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const input = document.querySelector('input');
+const btn = document.querySelector('.addTask > button');
+
+btn.addEventListener('click', addList);
+
+function addList(e){
+	const notCompleted = document.querySelector('.notCompleted');
+	const completed = document.querySelector('.completed');
+
+	const newLi = document.createElement('li');
+	const checkBtn = document.createElement('button');
+	const dltBtn = document.createElement('button');
+
+	checkBtn.innerHTML = '<i class="fa fa-check"></i>';
+	dltBtn.innerHTML = '<i class="fa fa-trash"></i>';
+
+	if(input.value !==''){
+		newLi.textContent = input.value;
+		input.value = '';
+		notCompleted.appendChild(newLi);
+		newLi.appendChild(checkBtn);
+		newLi.appendChild(dltBtn);
+	}
+	checkBtn.addEventListener('click', function(){
+		const parent = this.parentNode;
+		parent.remove();
+		completed.appendChild(parent);
+		checkBtn.style.display = 'none';
+	});
+
+	dltBtn.addEventListener('click', function(){
+		const parent = this.parentNode;
+		parent.remove();
+	});
+}
